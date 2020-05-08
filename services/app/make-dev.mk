e7e23d03e76be79a8d03b4f65c4f04572f3183cf
Naumenko Nikita <naumenkoniki@gmail.com>
2020-05-08 20:21:58 +0300

    fix envs

--- services/app/make-dev.mk
+++ services/app/make-dev.mk
@@ -1,10 +1,13 @@
--include ../../.env
-export $(shell sed 's/=.*//' ../../.env)
+-include ../../.env.prod
+export $(shell sed 's/=.*//' ../../.env.prod)
 
 prepare-db:
 	dropdb dev-db --if-exists
 	createdb dev-db
 	CLJ_ENV=development clojure -Amigratus
+
+run-migrations:
+	clojure -Amigratus
 
 repl:
 	iced repl
